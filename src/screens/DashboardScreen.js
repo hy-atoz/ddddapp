c07861839feea49e95d6be50c23d31c17f57df9b
hy-atoz <hooiyan.atoz@gmail.com>
2022-07-14 10:20:13 +0800

    Add dashboard draggable list

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,16 +1,21 @@
+import {HStack, Image, Pressable, Text, VStack} from 'native-base';
 import React, {useLayoutEffect, useState} from 'react';
-import {ScrollView} from 'native-base';
-import DashboardRow from '../components/DashboardRow';
-import c from '../constants/companies';
-import CustomButton from '../components/CustomButton';
+import {Dimensions} from 'react-native';
 import DraggableFlatList, {
-  ScaleDecorator,
-  RenderItemParams,
+  OpacityDecorator,
 } from 'react-native-draggable-flatlist';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import Icon from 'react-native-vector-icons/Entypo';
+import {useSelector} from 'react-redux';
+import CustomButton from '../components/CustomButton';
+import WinnerBadge from '../components/WinnerBadge';
+import c from '../constants/companies';
+import getItem from '../utils/getItem';
+
+const DEVICE_WIDTH = Dimensions.get('window').width;
 
 const DashboardScreen = ({navigation}) => {
   const [reorderMode, setReorderMode] = useState(false);
+  const result = useSelector(state => state.result.value);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -23,18 +28,90 @@ const DashboardScreen = ({navigation}) => {
     });
   }, [navigation, reorderMode]);
 
+  const renderItem = ({item, drag, isActive}) => {
+    const fdData = getItem(result, item.code).fdData;
+    const {day, dn, n1, n2, n3} = fdData;
+
+    return (
+      <OpacityDecorator>
+        <Pressable disabled={isActive} onPressIn={reorderMode ? drag : null}>
+          <HStack
+            alignItems="center"
+            borderBottomWidth="0.5"
+            borderBottomColor="muted.300"
+            justifyContent="space-between"
+            paddingX={DEVICE_WIDTH <= 320 ? 2 : 4}
+            paddingY={3}>
+            <HStack alignItems="center" space={4}>
+              <Image
+                alt={item.name_en}
+                height={DEVICE_WIDTH <= 320 ? 8 : 10}
+                width={DEVICE_WIDTH <= 320 ? 8 : 10}
+                source={item.image}
+              />
+              <VStack space={1}>
+                <Text
+                  color="black"
+                  fontSize={DEVICE_WIDTH <= 320 ? 'xs' : 'sm'}
+                  fontWeight="bold">
+                  {item.name_en} [{dn || '---/--'}] [{day || '---'}]
+                </Text>
+                <HStack space={DEVICE_WIDTH <= 320 ? 4 : 6}>
+                  <HStack alignItems="center">
+                    <WinnerBadge isFirst num="1" />
+                    <Text
+                      color="gray.700"
+                      fontFamily="Roboto-Bold"
+                      fontSize={DEVICE_WIDTH <= 320 ? 'xs' : 'md'}
+                      fontWeight="bold">
+                      {n1 || '----'}
+                    </Text>
+                  </HStack>
+                  <HStack alignItems="center">
+                    <WinnerBadge num="2" />
+                    <Text
+                      color="gray.700"
+                      fontFamily="Roboto-Bold"
+                      fontSize={DEVICE_WIDTH <= 320 ? 'xs' : 'md'}
+                      fontWeight="bold">
+                      {n2 || '----'}
+                    </Text>
+                  </HStack>
+                  <HStack alignItems="center">
+                    <WinnerBadge num="3" />
+                    <Text
+                      color="gray.700"
+                      fontFamily="Roboto-Bold"
+                      fontSize={DEVICE_WIDTH <= 320 ? 'xs' : 'md'}
+                      fontWeight="bold">
+                      {n3 || '----'}
+                    </Text>
+                  </HStack>
+                </HStack>
+              </VStack>
+            </HStack>
+            {reorderMode ? (
+              <Icon name="menu" size={24} color="#000" />
+            ) : (
+              <Icon name="chevron-small-right" size={24} color="gray" />
+            )}
+          </HStack>
+        </Pressable>
+      </OpacityDecorator>
+    );
+  };
+
   return (
-    <ScrollView alwaysBounceVertical={false}>
-      {c.map(company => (
-        <DashboardRow
-          key={company.id}
-          companyCode={company.code}
-          companyName={company.name_en}
-          inReorderMode={reorderMode}
-          source={company.image}
-        />
-      ))}
-    </ScrollView>
+    <DraggableFlatList
+      data={c}
+      onDragEnd={({data}) => {
+        // setData(data);
+        // storeData(data);
+        console.log(data);
+      }}
+      keyExtractor={item => item.id}
+      renderItem={renderItem}
+    />
   );
 };
 
